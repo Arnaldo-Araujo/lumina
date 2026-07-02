ec057f794fbd3acbe77d983253f426d658bec0a5
Arnaldo Araujo <arnaldoaraujo@unitins.br>
2026-07-02 00:00:02 -0300

    fix: adicionado espacos em entre palavras que estavam juntas.

--- src/main/java/dev/langchain4j/quarkus/workshop/MultiAgentWebSocket.java
+++ src/main/java/dev/langchain4j/quarkus/workshop/MultiAgentWebSocket.java
@@ -21,10 +21,10 @@ public class MultiAgentWebSocket {
     @OnOpen
     public String onOpen() {
         return "👋 Olá! Antes de tudo, quero agradeçer e me despedir temporariamente"
-        + "dos professores Fabio e Rodrigo e agradecer aos dois pelos ensinamentos" 
+        + " dos professores Fabio e Rodrigo e agradecer aos dois pelos ensinamentos" 
         + " passados, acredito que cresci bastante como aluno. Agora pode começar e"
-        + "perguntar sobre o processo já ingerido no sistema, 1 pdf, 1 docx e 1 txt," 
-        + "no menu a esquerda escolha o Agente que vai falar sobre o processo";
+        + " perguntar sobre o processo já ingerido no sistema, 1 pdf, 1 docx e 1 txt," 
+        + " no menu a esquerda escolha o Agente que vai falar sobre o processo";
     }
 
     @OnTextMessage
